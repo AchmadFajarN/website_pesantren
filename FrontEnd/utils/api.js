8817dfd080d44408587c12431c0c823c0b1010a1
ajay <achmadfajar712@gmail.com>
2025-06-30 13:02:08 +0700

    implemented login & logout

--- FrontEnd/utils/api.js
+++ FrontEnd/utils/api.js
@@ -1,93 +1,92 @@
 const BASE_URL = "";
 
 const getAccesToken = () => {
-  return JSON.parse(localStorage.getItem("accessToken"));
+  return localStorage.getItem("accessToken");
 };
-const putAccesToken = (token) => {
-  return localStorage.setItem("accessToken", token);
+const putAccesToken = (key, value) => {
+  return localStorage.setItem(key, value);
 };
 
-// fetch with token
-const fetchWithToken = async (url, options = {}) => {
-  const tokenObj = getAccesToken();
-  const token = tokenObj.token
-  return fetch(url, {
-    ...options,
-    headers: {
-      ...options.headers,
-      Authorization: `Bearer ${ token }`,
-    },
-  });
-};
+const removeLocalStorage = (key) => {
+  return localStorage.removeItem(key)
+} 
 
 // register
 const register = async ({ username, password, email }) => {
-  const res = await fetch(`${BASE_URL}/api/auth/register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      password,
-      email,
-    }),
-  });
-  const result = await res.json();
-  const { error } = result;
+  try {
+    const response = await fetch(`${BASE_URL}/api/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+      }),
+    });
 
-  if (!error) {
-    return {
-      result 
-    };
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    return err;
   }
-  return { error: true, message: error };
 };
 
 // login
 const login = async ({ username, password }) => {
-  const res = await fetch(`${BASE_URL}/api/auth/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  });
+  try {
+    const response = await fetch(`${BASE_URL}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
 
-  const result = await res.json();
-  const { error, token, user } = result;
-
-  if (!error) {
-    return {
-      data: {
-        token,
-        user: user.username
-      }
-    };
+    const result = response.json();
+    return result;
+  } catch (err) {
+    return err;
   }
-
-  return { error: error };
 };
 
 const registerStudent = async (biodata) => {
   try {
-    const req = await fetchWithToken(`${BASE_URL}/api/students`, {
-      method: 'POST',
+    const response = await fetch(`${BASE_URL}/api/students`, {
+      method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        Authorization: `Bearer ${getAccesToken()}`,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(biodata),
     });
 
-    const res = await req.json();
-    return res
+    const result = response.json();
+    return result;
   } catch (err) {
-    return { error: err.message }
+    return err;
   }
 };
 
-export { register, login, putAccesToken, getAccesToken, registerStudent };
+const getStudentByCredentials = async() => {
+  try {
+    const response = await fetch(`${BASE_URL}/api/students/my-registration`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${getAccesToken()}`,
+        "Content-Type": "applications/json"
+      }
+    });
+    const result = response.json();
+    return result;
+  } catch(err) {
+    return err
+  }
 
+}
+
+export { register, login, putAccesToken, removeLocalStorage, getAccesToken, registerStudent, getStudentByCredentials };
